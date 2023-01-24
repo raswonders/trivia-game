1cde35cf99727eaeb76b0caa554ea5f9f46ed19b
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-01-24 17:18:43 +0100

    add folder for static content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 8080;
+
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
 });
