c89affe710adae93deba5415ff215d281fb52dff
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-01-24 17:50:48 +0100

    add Socket.IO to Express server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
+const server = http.createServer(app); // create HTTP server using app
+const io = socketio(server); // connect Socket.IO to server
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('A new player just connected');
+})
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
 });
