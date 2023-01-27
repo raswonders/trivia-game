86c2c4d437429c7030f53f4a22a37d3f49b9c85d
Rastislav Hepner <rastislav.hepner@gmail.com>
2023-01-27 22:16:22 +0100

    add chat functionality

--- src/index.js
+++ src/index.js
@@ -65,6 +65,20 @@ io.on('connection', (socket) => {
       });
     }
   })
+
+  socket.on("sendMessage", (message, callback) => {
+    const { error, player } = getPlayer(socket.id);
+
+    if (error) return callback(error.message);
+
+    if (player) {
+      io.to(player.room).emit(
+        "message",
+        formatMessage(player.playerName, message)
+      );
+      callback();
+    }
+  })
 });
 
 
